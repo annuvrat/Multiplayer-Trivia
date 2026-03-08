59d91648a1a3263b8f1fc2434e7847cd71c54fce
annuvrat <annuvrat1098@gmail.com>
2026-03-08 21:37:36 +0530

    socket.ts

--- src/websockets/socket.ts
+++ src/websockets/socket.ts
@@ -1,41 +1,59 @@
 import { Socket, Server } from "socket.io";
 
+type SocketData = {
+  roomId?: string
+  userId?: string
+}
+
 export default function setupSocket(io: Server) {
 
   io.on("connection", (socket: Socket) => {
 
     console.log("User connected:", socket.id);
 
-    socket.on("join_room", ({ roomId , userId }) => {
+    socket.on("join_room", ({ roomId, userId }) => {
 
-      socket.join(roomId);
+      if (!roomId || !userId) return
 
-      socket.data.roomId = roomId;
-      socket.data.userId = userId;
+      socket.join(roomId)
 
-      console.log(`${userId} joined room ${roomId}`);
+      socket.data.roomId = roomId
+      socket.data.userId = userId
+
+      console.log(`${userId} joined room ${roomId}`)
 
       io.to(roomId).emit("player_joined", {
         userId,
         socketId: socket.id
-      });
+      })
 
-    });
+    })
+
+    socket.on("leave_room", ({ roomId, userId }) => {
+
+      socket.leave(roomId)
+
+      io.to(roomId).emit("player_left", {
+        userId,
+        socketId: socket.id
+      })
+
+    })
 
     socket.on("disconnect", () => {
 
-      const { roomId, userId } = socket.data;
+      const { roomId, userId } = socket.data as SocketData
 
-      if (roomId) {
+      if (roomId && userId) {
         io.to(roomId).emit("player_left", {
           userId,
           socketId: socket.id
-        });
+        })
       }
 
-      console.log("User disconnected:", socket.id);
-    });
+      console.log("User disconnected:", socket.id)
+    })
 
-  });
+  })
 
 }
